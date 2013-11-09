c97e0e2259737c6efa5793c178ea8cebe5f00498
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2013-11-09 23:08:44 +0100

    reset db config

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'dotapicks',
-			'username'  => 'dotapicks',
-			'password'  => 'dotapicks',
+			'database'  => '',
+			'username'  => '',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
